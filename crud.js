d0923222e508315968efec29a8b4c562463c4af1
Honchar Denys <crackeraki@gmail.com>
2020-02-09 13:49:10 +0200

    added code from express, crud and derer

---
+++ crud.js
@@ -0,0 +1,254 @@
+module.exports = function(waw) {
+	/*
+	*	Crud Fill
+	*/
+		var fill_crud = function(part, which, config){
+			var prefix = which+'_'+part+(config.name&&('_'+config.name)||'');
+			if(typeof config.ensure == 'function'){
+				waw['ensure_'+prefix] = config.ensure;
+			}
+			if(typeof config.query == 'function'){
+				waw['query_'+prefix] = config.query;
+			}
+			if(typeof config.sort == 'function'){
+				waw['sort_'+prefix] = config.sort;
+			}
+			if(typeof config.skip == 'function'){
+				waw['skip_'+prefix] = config.skip;
+			}
+			if(typeof config.limit == 'function'){
+				waw['limit_'+prefix] = config.limit;
+			}
+			if(typeof config.select == 'function'){
+				waw['select_'+prefix] = config.select;
+			}
+			if(typeof config.populate == 'function'){
+				waw['populate_'+prefix] = config.populate;
+			}
+		}
+		const crudTypes = ['create', 'get', 'update', 'unique', 'delete'];
+		waw.crud = function(part, config){
+			for (let i = 0; i < crudTypes.length; i++) {
+				if(Array.isArray(config[crudTypes[i]])){
+					for (let j = 0; j < config[crudTypes[i]].length; j++) {
+						if(typeof config[crudTypes[i]][j] != 'object') continue;
+						fill_crud(part, crudTypes[i], config[crudTypes[i]][j]);
+					}
+				}else if(typeof config[crudTypes[i]] == 'object'){
+					fill_crud(part, crudTypes[i], config[crudTypes[i]]);
+				}
+			}
+		};
+	/*
+	*	Crud Use
+	*/
+		const ensure = function(name){
+			return function(req, res, next){
+				if(typeof waw[name] == 'function'){
+					waw[name](req, res, next);
+				}else{
+					waw.ensure(req, res, next);
+				}
+			}
+		}
+		var add_crud = function(crud, part, unique=true){
+			var partName = part.name.toLowerCase();
+			var crudName = crud.name.toLowerCase();
+			var Schema = process.cwd() + '/server/' + partName + '/schema_' + crudName+ '.js';
+			if(unique) Schema = process.cwd() + '/server/' + partName + '/schema.js';
+			if (!waw.fs.existsSync(Schema)) {
+				var data = waw.fs.readFileSync(__dirname+'/schema.js', 'utf8');
+				data = data.replace('REPLACE', waw.capitalize(crudName));
+				waw.fs.writeFileSync(Schema, data, 'utf8');
+			}
+			Schema = require(Schema);
+			var router = waw.router('/api/'+crudName);
+			var save = function(doc, res){
+				doc.save(function(err){
+					if(err){
+						console.log(err);
+						return res.json(false);
+					}
+					res.json(doc);
+				});
+			}
+			/*
+			*	Create
+			*/
+				router.post("/create", ensure('ensure_create_'+crudName), function(req, res) {
+					var doc = new Schema();
+					if(typeof doc.create !== 'function'){
+						return res.json(false);
+					}
+					doc.create(req.body, req.user, sd);
+					save(doc, res);
+				});
+			/*
+			*	Read
+			*/
+				var get_unique = {};
+				var crud_get = function(name){
+					if(get_unique[name]) return;
+					get_unique[name] = true;
+					var final_name = '_get_'+crudName;
+					if(name) final_name += '_'+name;
+					router.get("/get"+name, ensure('ensure'+final_name), function(req, res) {
+						var query = waw['query'+final_name]&&waw['query'+final_name](req, res)||{
+							moderators: req.user&&req.user._id
+						};
+						query = Schema.find(query);
+						var sort = waw['sort'+final_name]&&waw['sort'+final_name](req, res)||false;
+						if(sort){
+							query.sort(sort);
+						}
+						var skip = waw['skip'+final_name]&&waw['skip'+final_name](req, res)||false;
+						if(skip){
+							query.skip(skip);
+						}
+						var limit = waw['limit'+final_name]&&waw['limit'+final_name](req, res)||false;
+						if(limit){
+							query.limit(limit);
+						}
+						var select = waw['select'+final_name]&&waw['select'+final_name](req, res)||false;
+						if(select){
+							query.select(select);
+						}
+						var populate = waw['populate'+final_name]&&waw['populate'+final_name](req, res)||false;
+						if(populate){
+							query.populate(populate);
+						}
+						query.exec(function(err, docs) {
+							if(err){
+								console.log(err);
+							}
+							res.json(docs || []);
+						});
+					});
+				}
+				if(Array.isArray(crud.get)){
+					for (var i = 0; i < crud.get.length; i++) {
+						crud_get(crud.get[i]);
+					}
+				}else if(typeof crud.get == 'string') crud_get(crud.get[i]);
+				else crud_get('');
+			/*
+			*	Update
+			*/
+				var crud_update = function(upd){
+					let final_name = '_update_'+crudName;
+					if(upd.name) final_name += '_'+upd.name;
+					router.post("/update"+(upd.name||''), ensure('ensure'+final_name), function(req, res) {
+						Schema.findOne(waw['query'+final_name]&&waw['query'+final_name](req, res)||{
+							_id: req.body._id,
+							moderators: req.user&&req.user._id
+						}, function(err, doc){
+							if(err||!doc){
+								err&&console.log(err);
+								return res.json(false);
+							}
+							for (var i = 0; i < upd.keys.length; i++) {
+								doc[upd.keys[i]] = req.body[upd.keys[i]];
+								doc.markModified(upd.keys[i]);
+							}
+							save(doc, res);
+						});
+					});
+				}
+				if(Array.isArray(crud.update)){
+					for (var i = 0; i < crud.update.length; i++) {
+						crud_update(crud.update[i]);
+					}
+				}else if(typeof crud.update == 'object') crud_update(crud.update);
+			/*
+			*	Unique
+			*/
+				var crud_unique = function(upd){
+					let final_name = '_unique_'+crudName;
+					if(upd.name) final_name += '_'+upd.name;
+					router.post("/unique"+(upd.name||''), ensure('ensure'+final_name), function(req, res) {
+						Schema.findOne(waw['query'+final_name]&&waw['query'+final_name](req, res)||{
+							_id: req.body._id,
+							moderators: req.user&&req.user._id
+						}, function(err, doc){
+							if(err||!doc) return res.json(false);
+							let query = waw['select'+final_name]&&waw['select'+final_name](req, res, upd);
+							if(!query){
+								query = {};
+								query[upd.key] = req.body[upd.key];
+							}
+							Schema.findOne(query, function(err, sdoc){
+								if(sdoc) return res.json(doc[upd.key]);
+								doc[upd.key] = req.body[upd.key];
+								doc.markModified(upd.key);
+								doc.save(function(err){
+									res.json(doc[upd.key]);
+								});
+							});
+						});
+					});
+				}
+				if(Array.isArray(crud.unique)){
+					for (var i = 0; i < crud.unique.length; i++) {
+						crud_unique(crud.unique[i]);
+					}
+				}else if(typeof crud.unique == 'object') crud_unique(crud.unique);
+			/*  
+			*	Delete
+			*/
+				var crud_delete = function(name){
+					let final_name = '_delete_'+crudName;
+					if(name) final_name += '_'+name;
+					router.post("/delete"+name, ensure('ensure'+final_name), function(req, res) {
+						let q = Schema.findOne(waw['query' + final_name] && waw['query' + final_name](req, res) || {
+							_id: req.body._id,
+							author: req.user._id
+						})
+						let populate = waw['populate'+final_name]&&waw['populate'+final_name](req, res)||false;
+						if(populate){
+							q.populate(populate);
+						}
+						q.exec(function(err, doc) {
+							if(err||!doc) return res.json(false);
+							Schema.deleteOne(waw['query' + final_name] && waw['query' + final_name](req, res) || {
+								_id: req.body._id,
+								author: req.user._id
+							}, function(err) {
+								if (err){
+									res.json(false);
+								}else{
+									if(typeof waw['on'+name] == 'function'){
+										waw['on'+name](doc, req, res);
+									}
+									res.json(true);
+								}
+							});
+						});
+					});
+				}
+				if(crud.delete){
+					for (var i = 0; i < crud.delete.length; i++) {
+						crud_delete(crud.delete[i]||'');
+					}
+				}
+			/*
+			*	End of CRUD
+			*/
+		}
+		for (var i = 0; i < waw.parts.length; i++) {
+			if(waw.parts[i].crud){
+				if(Array.isArray(waw.parts[i].crud)){
+					for (var j = 0; j < waw.parts[i].crud.length; j++) {
+						add_crud(waw.parts[i].crud[j], waw.parts[i], false);
+					}
+				}else{
+					if(!waw.parts[i].crud.name){
+						waw.parts[i].crud.name = waw.parts[i].name
+					}
+					add_crud(waw.parts[i].crud, waw.parts[i]);
+				}
+			}
+		}
+	/*
+	*	Support for 0.x version of waw until 2.0
+	*/
+};
--- index.js
+++ index.js
@@ -1,1 +1,170 @@
-module.exports = function(waw){}
+const express = require('express');
+var session = require('express-session');
+const app = express();
+const server = require('http').Server(app);
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const derer = require('derer');
+module.exports = function(waw){
+	var sessionMaxAge = 365 * 24 * 60 * 60 * 1000;
+	if(typeof waw.config.session == 'number'){
+		sessionMaxAge = waw.config.session;
+	}	
+	var store;
+	if(waw.config.mongo){
+		let mongoAuth = '';
+		if(waw.config.mongo.user&&waw.config.mongo.pass){
+			mongoAuth = waw.config.mongo.user + ':' + waw.config.mongo.pass + '@';
+		}
+		waw.mongoUrl = 'mongodb://'+mongoAuth+(waw.config.mongo.host||'localhost')+':'+(waw.config.mongo.port||'27017')+'/'+(waw.config.mongo.db||'test');
+	}
+	if(waw.mongoUrl){
+		store = new(require("connect-mongo")(session))({
+			url: waw.mongoUrl
+		});
+	}
+	app.use(session({
+		key: 'express.sid.'+waw.config.prefix,
+		secret: 'thisIsCoolSecretFromWaWFramework'+waw.config.prefix,
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: sessionMaxAge,
+			domain: waw.config.domain||undefined
+		},
+		rolling: true,
+		store: store
+	}));
+	waw.store = store;
+
+	if(waw.config.icon && waw.fs.existsSync(process.cwd() + waw.config.icon))
+		app.use(favicon(process.cwd() + waw.config.icon));
+	app.use(cookieParser());
+	app.use(methodOverride('X-HTTP-Method-Override'));
+	app.use(bodyParser.urlencoded({
+		'extended': 'true',
+		'limit': '50mb'
+	}));
+	app.use(bodyParser.json({
+		'limit': '50mb'
+	}));
+	if(!waw.config.port) waw.config.port=8080;
+	server.listen(waw.config.port);
+	console.log("App listening on port " + (waw.config.port));
+	/*
+	*	Helpers
+	*/
+		waw.router = function(api){
+			var router = express.Router();
+			app.use(api, router);
+			return router;
+		}
+
+		waw.app = app;
+		waw.server = server;
+		waw.cookieParser = cookieParser;
+		waw.methodOverride = methodOverride;
+		waw.bodyParser = bodyParser;
+	/*
+	*	Use
+	*/
+		const use = [function(req, res, next){
+			req.url = req.originalUrl.toLowerCase().split('?')[0];
+			next();
+		}];
+		waw.use = function(func){
+			use.push(func);
+		}
+		app.use(function(req, res, next){
+			serial(use, next, function(func, nx){
+				if(typeof func == 'function') func(req, res, nx);
+				else nx();
+			});
+		});
+	/*
+	*	Middleware
+	*/
+		const middlewares = {};
+		waw.middleware = function(which, req, res, next){
+			if(typeof middlewares[which] == 'function'){
+				middlewares[which](req, res, next);
+			}else next({});
+		}
+		waw.set_middleware = function(which, cb){
+			if(typeof cb == 'function' && which){
+				middlewares[which] = cb;
+			}				
+		}
+	/*
+	*	Support for 0.x version of waw until 2.0
+	*/
+		waw._initRouter = waw.router;
+		waw._app = app;
+		waw.next = (req, res, next)=>next()
+		waw.ensure = (req, res, next)=>{
+			if(req.user) next();
+			else res.json(false);
+		}
+		waw._ensure = waw.ensure;
+		waw._config = waw.config;
+		waw._middleware = [];
+	/*
+	*	Move to helper
+	*/
+		const _serial = function(i, arr, callback, custom_call){
+			if(i>=arr.length) return callback();
+			if(typeof custom_call == 'function'){
+				custom_call(arr[i], function(){
+					_serial(++i, arr, callback, custom_call);
+				});
+			}else{
+				arr[i](function(){
+					_serial(++i, arr, callback, custom_call);
+				});
+			}
+		}
+		const serial = (arr, callback, custom_call) => _serial(0, arr, callback, custom_call);
+		waw.afterWhile = (timeout, cb, time)=>{
+			if(typeof timeout == 'function'){
+				if(typeof cb == 'number'){
+					time = cb;
+				}
+				cb = timeout;
+				timeout = this;
+			}
+			if(typeof time != 'number'){
+				time = 1000;
+			}
+			clearTimeout(timeout._timeout);
+			timeout._timeout = setTimeout(cb, time);
+		};
+	/*
+	*	Server Rendering
+	*/
+		waw.derer = derer;
+		var dererOpts = {
+			varControls: ['{{{', '}}}']
+		}
+		if(!waw.config.production){
+			dererOpts.cache = false;
+		}
+		derer.setDefaults(dererOpts);
+		waw.app.engine('html', derer.renderFile);
+		waw.app.set('view engine', 'html');
+		waw.app.set('view cache', true);
+		derer.setFilter('string',function(input){
+			return input&&input.toString()||'';
+		});		
+		derer.setFilter('fixlink',function(link){
+			if(link.indexOf('//')>0) return link;
+			else return 'http://'+link;
+		});
+		// derer.setFilter('tr', waw._tr);
+		// derer.setFilter('translate', waw._tr);
+		waw._derer = derer;
+	/*
+	*	End of
+	*/
+}
