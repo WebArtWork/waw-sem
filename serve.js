ca637588504ef2a08f90f6db0d4097fd0c540304
Honchar Denys <crackeraki@gmail.com>
2021-06-27 11:59:32 +0300

    fixed serve file

--- serve.js
+++ serve.js
@@ -55,7 +55,7 @@ module.exports = function(waw){
 		if(content[req.get('host').toLowerCase()+url]) return res.send(content[req.get('host').toLowerCase()+url]);
 		if(content[url]) return res.send(content[url]);
 		if(files[req.get('host').toLowerCase()+url]) return res.sendFile(files[req.get('host').toLowerCase()+url]);
-		if(files[url]) return res.send(files[url]);
+		if(files[url]) return res.sendFile(files[url]);
 		next();
 	});
 }
