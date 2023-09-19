4253ecef9d39d91655b8387b919ba30f8639cd1d
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-19 12:30:50 +0300

    Create sem.collection.js

---
+++ module/default/sem.collection.js
@@ -0,0 +1,34 @@
+module.exports = function (waw) {
+const mongoose = require('mongoose');
+
+const Schema = waw.mongoose.Schema({
+	name: String,
+	description: String,
+	data: {},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	moderators: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			sparse: true,
+			ref: 'User'
+		}
+	]
+});
+
+Schema.methods.create = function (obj, user, waw) {
+	this.author = user._id;
+
+	this.moderators = [user._id];
+
+	this.name = obj.name;
+
+	this.description = obj.description;
+
+	this.data = obj.data;
+}
+
+return waw.Form = waw.mongoose.model('CNAME', Schema);
+}
