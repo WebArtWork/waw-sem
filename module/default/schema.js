4040bf89dafab6910f85c21687ebdcc2fd528e73
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-30 12:33:06 +0300

    Update schema.js

--- module/default/schema.js
+++ module/default/schema.js
@@ -1,31 +1,31 @@
-module.exports = async function(waw) {
+module.exports = async function (waw) {
 	const Schema = waw.mongoose.Schema({
-	name: String,
-	description: String,
-	data: {},
-	author: {
-		type: waw.mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	moderators: [
-		{
+		name: String,
+		description: String,
+		data: {},
+		author: {
 			type: waw.mongoose.Schema.Types.ObjectId,
-			sparse: true,
-			ref: 'User'
-		}
-	]
-});
+			ref: "User",
+		},
+		moderators: [
+			{
+				type: waw.mongoose.Schema.Types.ObjectId,
+				sparse: true,
+				ref: "User",
+			},
+		],
+	});
 
-Schema.methods.create = function (obj, user, waw) {
-	this.author = user._id;
+	Schema.methods.create = function (obj, user, waw) {
+		this.author = user._id;
 
-	this.moderators = [user._id];
+		this.moderators = [user._id];
 
-	this.name = obj.name;
+		this.name = obj.name;
 
-	this.description = obj.description;
+		this.description = obj.description;
 
-	this.data = obj.data;
-}
-	return waw.CNAME = waw.mongoose.model('CNAME', Schema);
-}
+		this.data = obj.data;
+	};
+	return (waw.CNAME = waw.mongoose.model("CNAME", Schema));
+};
