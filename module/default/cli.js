62e537f1907925ec2a5c7055a77184e26326a513
Honchar Denys <crackeraki@gmail.com>
2023-10-20 01:11:47 +0300

    init registry

--- module/default/cli.js
+++ module/default/cli.js
@@ -1,22 +1,43 @@
+const fetch = require("node-fetch");
+const path = require('path');
 const fs = require('fs');
 
-module.exports = function (waw) {
+module.exports = async function (waw) {
 	fs.mkdirSync(waw.base, { recursive: true });
 
-	let file = fs.readFileSync(waw.template + '/index.js', 'utf8');
-	file = file.split('CNAME').join(waw.Name);
-	file = file.split('NAME').join(waw.name);
-	fs.writeFileSync(waw.base + `/${waw.name}.api.js`, file, 'utf8');
+	const response = await fetch(
+		"https://webart.work/api/registry/waw/module/" + waw.name
+	);
+	if (response.ok) {
+		const resp = await response.json();
 
-	file = fs.readFileSync(waw.template + '/schema.js', 'utf8');
-	file = file.split('CNAME').join(waw.Name);
-	file = file.split('NAME').join(waw.name);
-	fs.writeFileSync(waw.base + `/${waw.name}.collection.js`, file, 'utf8');
+		if (resp.repo) {
+			waw.fetch(waw.base, resp.repo, (err) => {}, resp.branch || 'master');
+		} else {
+			for (const file in resp.files) {
+				fs.writeFileSync(
+					path.join(waw.base, file),
+					resp.files[file],
+					'utf8'
+				);
+			}
+		}
+	} else {
+		let file = fs.readFileSync(waw.template + '/index.js', 'utf8');
+		file = file.split('CNAME').join(waw.Name);
+		file = file.split('NAME').join(waw.name);
+		fs.writeFileSync(waw.base + `/${waw.name}.api.js`, file, 'utf8');
 
-	file = fs.readFileSync(waw.template + '/module.json', 'utf8');
-	file = file.split('CNAME').join(waw.Name);
-	file = file.split('NAME').join(waw.name);
-	fs.writeFileSync(waw.base + '/module.json', file, 'utf8');
+		file = fs.readFileSync(waw.template + '/schema.js', 'utf8');
+		file = file.split('CNAME').join(waw.Name);
+		file = file.split('NAME').join(waw.name);
+		fs.writeFileSync(waw.base + `/${waw.name}.collection.js`, file, 'utf8');
+
+		file = fs.readFileSync(waw.template + '/module.json', 'utf8');
+		file = file.split('CNAME').join(waw.Name);
+		file = file.split('NAME').join(waw.name);
+		fs.writeFileSync(waw.base + '/module.json', file, 'utf8');
+	}
 
 	console.log('Module has been created');
 
