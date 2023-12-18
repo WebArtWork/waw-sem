aa3cda08d3b7d1ab25e5e9ba29cf433e1a530632
Nika <129855721+nikiquickie@users.noreply.github.com>
2023-12-18 15:37:58 +0200

    Update crud.wjst.js

--- crud.wjst.js
+++ crud.wjst.js
@@ -28,7 +28,6 @@ class Crud {
 	getName = '';
 	async page(page, name = '') {
 		try {
-			page++;
 			const response = await Http.get(`${this.api}/get${name || this.getName}?skip=${page * this.perPage}&limit=${this.perPage}`);
 			return response;
 		} catch (error) {
