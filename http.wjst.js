03c7e06d72c3a88176ed2e3eb838dab569220776
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-05-15 14:04:04 +0300

    Update http.wjst.js

--- http.wjst.js
+++ http.wjst.js
@@ -1,35 +1,59 @@
 class Http {
-	async get(url) {
-		try {
-			const response = await fetch(url);
-			if (!response.ok) {
-				throw new Error(`HTTP Error! Status: ${response.status}`);
-			}
-			return await response.json();
-		} catch (error) {
-			throw new Error(`Failed to fetch data: ${error.message}`);
-		}
-	}
+    headers = this.loadHeadersFromLocalStorage();
 
-	async post(url, data) {
-		try {
-			const response = await fetch(url, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			});
+    async get(url) {
+        try {
+            const response = await fetch(url, {
+                headers: this.headers
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP Error! Status: ${response.status}`);
+            }
+            return await response.json();
+        } catch (error) {
+            throw new Error(`Failed to fetch data: ${error.message}`);
+        }
+    }
 
-			if (!response.ok) {
-				throw new Error(`HTTP Error! Status: ${response.status}`);
-			}
+    async post(url, data) {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    ...this.headers,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
 
-			return await response.json();
-		} catch (error) {
-			throw new Error(`Failed to post data: ${error.message}`);
-		}
-	}
+            if (!response.ok) {
+                throw new Error(`HTTP Error! Status: ${response.status}`);
+            }
+
+            return await response.json();
+        } catch (error) {
+            throw new Error(`Failed to post data: ${error.message}`);
+        }
+    }
+
+    setHeader(header, value) {
+        this.headers[header] = value;
+        this.saveHeadersToLocalStorage();
+    }
+
+    removeHeader(header) {
+        delete this.headers[header];
+        this.saveHeadersToLocalStorage();
+    }
+
+    saveHeadersToLocalStorage() {
+        localStorage.setItem('httpHeaders', JSON.stringify(this.headers));
+    }
+
+    private loadHeadersFromLocalStorage() {
+        const storedHeaders = localStorage.getItem('httpHeaders');
+        return storedHeaders ? JSON.parse(storedHeaders) : {};
+    }
 }
 
 export default new Http();
