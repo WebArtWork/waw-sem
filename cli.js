b90ee0da740a55b074f72a06a0cde8142965bc8e
Honchar Denys <crackeraki@gmail.com>
2026-01-26 23:20:51 +0200

    small adjustments to follow new waw framework, module is broken

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 
-const new_module = require('./util.module');
+const new_module = require('waw/server/sem/util.module');
 
 module.exports.add = new_module;
 module.exports.a = new_module;
 
-const fetch_module = require('./util.module');
+const fetch_module = require('waw/server/sem/util.module');
 
 module.exports.fetch = fetch_module;
 module.exports.f = fetch_module;
